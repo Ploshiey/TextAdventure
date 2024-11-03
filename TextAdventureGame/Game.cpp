7646662d0ec5a2d07f9d01ab9437d27be1df7cc8
Ploshiey <maxkelly04@gmail.com>
2024-11-03 22:48:34 +1100

    more stuff

--- TextAdventureGame/Game.cpp
+++ TextAdventureGame/Game.cpp
@@ -2,37 +2,133 @@
 #include <iostream>
 using namespace std;
 
-struct item
-	{
-		string name;
-		float damage;
-		string effects;
-	};
+
+
+struct Item
+{
+	string name;
+	float damage;
+	string effects;
+};
+
+struct Enemy
+{
+	string name;
+	float health;
+	float damage;
+	string type;
+};
 
 struct Room
 {
-	int room[4][4];
 	bool cleared;
-	struct Enemy
+	Enemy enemy;
+	Item reward;
+};
+
+class playerMovement
+{
+public:
+	Room RoomMod[4][4];
+
+	void moveLeft()
 	{
-		string name;
-		float health;
-		float damage;
-		string type;
-	};
-	item reward;
+		
+	}
+	void moveRight()
+	{
+
+	}
+	void moveUp()
+	{
+
+	}
+	void moveDown()
+	{
+
+	}
 };
 
 struct Player
 {
 	float health;
 	float damage;
-	item heldItem;
+	Item heldItem;
+	int score;
 };
+
+Room room[4][4];
+int temp;
+
+Enemy posEnemies[15];
+
+string namePicker()
+{
+	temp = rand() % 10;
+	switch (temp)
+	{
+	case 0:
+		return "boobs";
+		break;
+	case 1:
+		return "boob";
+		break;
+	case 2:
+		return "boos";
+		break;
+	case 3:
+		return "bo";
+		break;
+	case 4:
+		return "bs";
+		break;
+	case 5:
+		return "bobs";
+		break;
+	case 6:
+		return "oobs";
+		break;
+	case 7:
+		return "obs";
+		break;
+	case 8:
+		return "b";
+		break;
+	case 9:
+		return "boobies";
+		break;
+	default:
+		return "error";
+		break;
+	}
+
+}
+
+void setup()
+{
+	for (int i = 0; i < 15; i++)
+	{
+		posEnemies[i].name = namePicker();
+	}
+	
+	
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			room[i][j].enemy.damage = rand() % 100 + 300;
+		}
+	}
+}
 
 int main()
 {
-	
+	setup();
+	for (int i = 0; i < 15; i++)
+	{
+		cout << posEnemies[i].name << endl;
+	}
 }
+
 
 
